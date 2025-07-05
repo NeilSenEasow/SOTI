0771b05c26492b0e969029efaf934262f20647f8
Neil Sen Easow <neilseneasow@gmail.com>
2025-07-05 23:25:52 +0530

    Add wave array

---
+++ Day5/waveArray.java
@@ -0,0 +1,14 @@
+public class Solution {
+    public ArrayList<Integer> wave(ArrayList<Integer> A) {
+        int n = A.size();
+        Collections.sort(A);
+        for(int i=0 ; i<n-1 ; i=+2)	{
+            //swap elements
+            int temp = A.get(i);
+            A.set(i,A.get(i+1));
+            A.set(i+1,temp);
+        
+        }
+        return A;
+    }
+}
