1cbf6955f6286e28c6e2dbaffe5e238288c8e6b7
Neil Sen Easow <neilseneasow@gmail.com>
2025-11-01 14:10:43 +0530

    area or perimeter

---
+++ Day16/AreaOrPerimeter.java
@@ -0,0 +1,27 @@
+package Day16;
+
+public class AreaOrPerimeter {
+    public static int areaOrPerimeter (int l, int w) {
+        if(l==w) {
+            return l*l;
+        }else{
+            return 2*(l+w);
+        }
+    }
+}
+
+public class AreaOrPerimeterTest {
+    public void test1() {
+        assertEquals(16,
+        AreaOrPerimeter.areaOrPerimeter(4, 4));
+    }
+    public void test2() {
+        assertEquals(14,
+        AreaOrPerimeter.areaOrPerimeter(6, 10));
+    }
+    private void assertEquals(int expected, int actual) {
+        if (expected != actual) {
+            throw new AssertionError("Expected: " + expected + " but was: " + actual);
+        }
+    }
+}
